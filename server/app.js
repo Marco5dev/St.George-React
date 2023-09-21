7e16d4ca8b864ea325ee65e3f3b306357a866603
Marco <100785809+Marco5dev@users.noreply.github.com>
2023-09-21 14:06:39 +0300

    Fix the API

--- server/app.js
+++ server/app.js
@@ -31,6 +31,16 @@ app.use(express.json());
 app.use(logger("dev"));
 app.use(expressip().getIpInfoMiddleware);
 app.use(cookieParser("Marco5dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(
   session({
     secret: "Marco5dev",
@@ -38,16 +48,6 @@ app.use(
     saveUninitialized: true,
   })
 );
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 
 // Routers
 app.use("/", homeRouter);
